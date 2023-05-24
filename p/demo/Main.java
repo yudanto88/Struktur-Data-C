3c79c23e88b908e521ba0b6362b8784ecfebe0e4
Yudanto Aditama <89512935+Kyr0adtm@users.noreply.github.com>
2023-05-24 21:38:27 +0700

    Latihan Modul 5
    codingan latihan untuk binary tree

---
+++ p/demo/Main.java
@@ -0,0 +1,20 @@
+package p.demo;
+
+public class Main {
+    public static void main(String[] args) {
+        BinaryTree pohon = new BinaryTree();
+
+        pohon.NewNode(20);
+        pohon.NewNode(2);
+        pohon.NewNode(25);
+        pohon.NewNode(37);
+        pohon.NewNode(16);
+
+        System.out.println("\nPreOrder =");
+        pohon.preOrder(pohon.root);
+        System.out.println("\nInOrder =");
+        pohon.inOrder(pohon.root);
+        System.out.println("\nPostOrder =");
+        pohon.postOrder(pohon.root);
+    }
+}
